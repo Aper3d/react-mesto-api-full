eddd2b0461980691ecf08ea74705631f42bfb67c
Oleg Dmitriev <oleg_surgut@hotmail.com>
2022-09-01 13:41:58 +0500

    pr15fix4.2

--- backend/app.js
+++ backend/app.js
@@ -13,16 +13,18 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(cors());
-app.use(helmet());
-app.use(express.json());
-
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   autoIndex: true,
 });
 
+app.use(cors({
+  Origin: 'https://legion3d.students.nomoredomainssbs.ru',
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
 app.use(requestLogger);
 
 app.get('/crash-test', () => {
